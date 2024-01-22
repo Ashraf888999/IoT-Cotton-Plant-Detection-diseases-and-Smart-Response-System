f4dff50d59458ba1483ba719a97abc53a52e9d93
Ashraf888999 <157127037+Ashraf888999@users.noreply.github.com>
2024-01-22 18:58:58 +0200

    Update DHT11_sensor.ino
    add the pin connected to the DHT sensor

--- sketch_one_/DHT11_sensor.ino
+++ sketch_one_/DHT11_sensor.ino
@@ -1,7 +1,8 @@
 
 #include <DHT.h>
 
-
+#define DHTPIN 2       // Pin connected to the DHT sensor
+#define DHTTYPE DHT11  // Type of the DHT sensor
 
 DHT dht(DHTPIN, DHTTYPE);  // Initialize the DHT sensor
 
